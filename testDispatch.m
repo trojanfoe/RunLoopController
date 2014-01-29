83d8f2fdd0853be4af341cbe058ae1222fe291e2
Andy Duplain <andy@trojanfoe.com>
2014-01-29 15:34:47 +0000

    Add more to testDispatch.

--- testDispatch.m
+++ testDispatch.m
@@ -15,16 +15,30 @@ int main(int argc, const char **argv) {
         
         __block NSInteger count = 0;
         dispatch_queue_t q = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-        for (unsigned i = 0; i < 4; i++) {
-            count++;
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (i + 1) * NSEC_PER_SEC), q, ^{
-                LOG(@"Block %u fired", i);
-                count--;
 
-                // signal is used to get the main thread's run loop to terminate, allowing
-                // the code below to check if all work is complete
-                [[RunLoopController mainRunLoopController] signal];
-            });
+        for (unsigned i = 0; i < 8; i++) {
+            LOG(@"Dispatching block %u", i);
+            count++;
+            if (i < 4) {
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (i + 1) * NSEC_PER_SEC), q, ^{
+                    LOG(@"Block %u fired", i);
+
+                    // Decrement the count and signal the main thread's run loop, allowing
+                    // the code below to check if all work is complete
+                    count--;
+                    [[RunLoopController mainRunLoopController] signal];
+                });
+            } else {
+                dispatch_async(q, ^{
+                    [NSThread sleepForTimeInterval:i - 3.0f]; 
+                    LOG(@"Block %u finished", i);
+
+                    // Decrement the count and signal the main thread's run loop, allowing
+                    // the code below to check if all work is complete
+                    count--;
+                    [[RunLoopController mainRunLoopController] signal];
+                });
+            }
         }
 
         while ([runLoopController run]) {
