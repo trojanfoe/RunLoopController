5dc91366874b0a5b93b8b4d63f03b4a46b6fe2a1
Alex Gray <alex@mrgray.com>
2015-03-12 12:15:33 -0400

    also made comments match apple's current guidelines and doc'd new shouldTerminate property

--- RunLoopController.h
+++ RunLoopController.h
@@ -6,32 +6,42 @@
  * Licensed under the MIT License.
  */
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @interface RunLoopController : NSObject <NSMachPortDelegate>
 
+/*! Set to YES to set terminate flag and signal the associated 
+    run loop (calling the @c -signal method).
+    @return YES if the run loop terminate flag/should terminate, else NO.
+ */
 @property (nonatomic) BOOL shouldTerminate;
 
 /*! The number of registered instances.
-    @note This value can be used to decide to terminate the main thread when no more worker threads exist.
+    @note This value can be used to decide to terminate 
+          the main thread when no more worker threads exist.
  */
 + (NSInteger) instanceCount;
 
 /// The RunLoopController object associated with the current thread.
+
 + (RunLoopController*) currentRunLoopController;
 
 /// The RunLoopController object associated with the main thread.
+
 + (RunLoopController*) mainRunLoopController;
 
 /// Register the run loop controller with the current run loop.
+
 - (void) register;
 
 /// Deregister the run loop controller from the current run loop.
+
 - (void) deregister;
 
 /*! Run the current run loop.  
-    @note This is a shortcut for: @see runMode:NSDefaultRunLoopModebeforeDate: with NSDate.distantFuture  
-    @return NO if the run loop was asked to terminate, or an error occurred. 
+    @note This is a shortcut for: 
+    @see runMode:NSDefaultRunLoopModebeforeDate: with NSDate.distantFuture
+    @return NO  if the run loop was asked to terminate, or an error occurred.
             YES if the run loop finished for another reason.
  */
 - (BOOL) run;
@@ -39,16 +49,10 @@
 /*! Run the current run loop.
     @param mode       As per [NSRunLoop runMode:limitDate:].
     @param limitDate  As per [NSRunLoop runMode:limitDate:].
-    @return NO        if the run loop was asked to terminate, or an error occurred.
-            YES       if the run loop finished for another reason.
+    @return  NO       the run loop was asked to terminate, or an error occurred.
+            YES       the run loop finished for another reason.
  */
-- (BOOL)runMode:(NSString*)mode beforeDate:(NSDate*)limitDate;
-
-/// Set the terminate flag and signal the associated run loop (calling the @c -signal method).
-//- (void) terminate;
-
-/// Determine if the terminate flag is set. YES if the run loop should terminate, else NO.
-//- (BOOL) shouldTerminate;
+- (BOOL) runMode:(NSString*)mode beforeDate:(NSDate*)limitDate;
 
 /*! Signal the run loop associated with the run loop controller.
     @note This is useful if you want to wake up a run loop so the code running the loop 
